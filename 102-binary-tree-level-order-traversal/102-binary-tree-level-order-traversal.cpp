24230608511b6baad1a72821433a86d2c3e7c1a7
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2022-06-03 10:59:45 +0600

    Time: 7 ms (48.62%), Space: 12.5 MB (84.58%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,38 +12,27 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int> > output;
-        vector<int> temp;
-        queue<pair<TreeNode*, int> >q;
-        
-        if(!root)
-            return output;
-        
-        q.push(make_pair(root, 0));
-        
-        
-        
-        while(!q.empty()){
-            TreeNode* node = q.front().first;
-            int level = q.front().second;
-            if(output.size()>level){
-                output[level].push_back(node->val);
-            }
-            else{
-                temp.push_back(node->val);
-                output.push_back(temp);
-                temp.clear();
-            }
+        vector<vector<int> >  result;
+        if (!root) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> cur_vec;
+        while(!q.empty()) {
+            TreeNode* t = q.front();
             q.pop();
-            if(node->left != nullptr){
-                q.push(make_pair(node->left, level+1));
+            if (t==NULL) {
+                result.push_back(cur_vec);
+                cur_vec.resize(0);
+                if (q.size() > 0) {
+                    q.push(NULL);
+                }
+            } else {
+                cur_vec.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
             }
-                
-            if(node->right != nullptr){
-                q.push(make_pair(node->right, level+1));
-            }
-            
         }
-        return output;
+        return result;
     }
 };
