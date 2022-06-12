f4e6f7481ef038cee1c25ca95d740e6e8143ccd4
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2022-06-12 22:57:37 +0600

    Time: 15 ms (49.51%), Space: 11.1 MB (33.47%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -8,18 +8,18 @@ public:
             return mat;
         }
         else{
-            vector<vector<int>>v;
-            vector<int>temp;
-            int s;
-            s=0;
+            vector<vector<int>>v(r);
+            
+            int s,l;
+            s=l=0;
             for(int i=0;i<m;i++){
                 for(int j=0;j<n;j++){
-                    temp.push_back(mat[i][j]);
+                    v[l].push_back(mat[i][j]);
                     s++;
+                    
                     if(s==c){
                         s=0;
-                        v.push_back(temp);
-                        temp.clear();
+                        l++;
                     }
                 }
             }
