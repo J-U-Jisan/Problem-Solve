b94cac750d092a177f8bc942bba73686b94f12eb
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2022-06-17 10:30:55 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (96.07%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==')'){
-                if(!st.empty() && st.top() == '(')
-                    st.pop();
-                else
-                    return false;
+        int top = -1;
+        for(int i =0;i<s.length();++i){
+            if(top<0 || !isMatch(s[top], s[i])){
+                ++top;
+                s[top] = s[i];
+            }else{
+                --top;
             }
-            else if(s[i] == '}'){
-                if(!st.empty() && st.top()=='{')
-                    st.pop();
-                else
-                    return false;
-            }
-            else if(s[i] == ']'){
-                if(!st.empty() && st.top()=='[')
-                    st.pop();
-                else
-                    return false;
-            }
-            else
-                st.push(s[i]);
         }
-        if(st.empty())
-            return true;
-        
+        return top == -1;
+    }
+    bool isMatch(char c1, char c2){
+        if(c1 == '(' && c2 == ')') return true;
+        if(c1 == '[' && c2 == ']') return true;
+        if(c1 == '{' && c2 == '}') return true;
         return false;
     }
 };
