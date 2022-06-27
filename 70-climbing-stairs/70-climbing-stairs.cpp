0c3cbb10dedbc560373f98d3f480d7199de65eb1
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2022-06-27 16:52:49 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (55.90%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,16 +3,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         int dp[46];
-        memset(dp, -1 , sizeof(dp));
-        return noOfWays(n,dp);
-    }
-    int noOfWays(int n, int dp[]){
-        if(n<0) return 0;
-        if(n==0)
-            return 1;
-        if(dp[n] != -1)
-            return dp[n];
+        dp[0] = 1;
+        for(int i= 1;i<=n;i++){
+            
+            dp[i] = dp[i-1];
+            if(i>1)
+                dp[i]+=dp[i-2];
+        }   
         
-        return dp[n] = noOfWays(n-1, dp) + noOfWays(n-2, dp);
+        return dp[n];
     }
 };
