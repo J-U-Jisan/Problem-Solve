a434b3fe19e25d7a5657a1772a97363cd2ffda81
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2022-06-27 16:15:54 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (79.77%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0, b = 1;
+        if(n==0)
+            return a;
+        else{
+            while(--n){
+                b = a+b;
+                a = b - a;
+            }
+            return b;
+        }
+    }
+};
